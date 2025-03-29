e7d5e22e8cce8896a9c16600863cef9ad1f94bb6
MouradK2003 <mourad.ketata@utbm.fr>
2025-03-29 11:58:20 +0100

    update login

--- login.php
+++ login.php
@@ -2,16 +2,25 @@
 $styles = ['register_login_style'];
 include("PageParts/header.php");
 ?>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
 <div class="form-container">
 <h3 class="subtitle">Log in</h3>
     <form class="reg-log-form" action="?" method="POST"> <!-- action="x.php" method="POST" -->
 
-        <label>Email :</label>
-        <input type="email" required />
+        <label>Email</label>
+        <div class="input-wrapper">
+            <input type="email" />
+            <i class="fas fa-envelope input-icon"></i>
+        </div>
 
-        <label>Password :</label>
-        <input type="password" required />
+        <label>Password</label>
+        <div class="input-wrapper">
+            <input type="password" />
+            <i class="fas fa-lock input-icon"></i>
+        </div>
+
+
 
         <button type="submit" class="startnow buttonlink" class="submit-reg">Sign up</button>
     </form>
