6c8a183a575df6de00c504392c0ebde8edfc4c80
zshelali <ali.hajeri24@gmail.com>
2025-03-29 13:17:51 +0100

    .htaccess added (redirect 404 errors for apache)

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php
+$styles = ['index_style'];
+include("PageParts/header.php");
+?>
+
+<h1 style="text-align: center; font-size: 40px;">404 ! This is NOT the page you were looking for...</h1>
+
+
+<?php
+include("PageParts/footer.php");
+?>
