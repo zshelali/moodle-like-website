af9cead5eeba6588ac3267110ead97cfb2266738
zshelali <ali.hajeri24@gmail.com>
2025-03-29 12:12:37 +0100

    restored

--- login.php
+++ login.php
@@ -2,27 +2,18 @@
 $styles = ['register_login_style'];
 include("PageParts/header.php");
 ?>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
 <div class="form-container">
-<h3 class="subtitle">Welcome back</h3>
+<h3 class="subtitle">Log in</h3>
     <form class="reg-log-form" action="?" method="POST"> <!-- action="x.php" method="POST" -->
 
-        <label>Email</label>
-        <div class="input-wrapper">
-            <input type="email" />
-            <i class="fas fa-envelope input-icon"></i>
-        </div>
+        <label>Email :</label>
+        <input type="email" required />
 
-        <label>Password</label>
-        <div class="input-wrapper">
-            <input type="password" />
-            <i class="fas fa-lock input-icon"></i>
-        </div>
+        <label>Password :</label>
+        <input type="password" required />
 
-
-
-        <button type="submit" class="buttonlink submit-reg">Log in</button>
+        <button type="submit" class="startnow buttonlink" class="submit-reg">Sign up</button>
     </form>
 </div>
 
