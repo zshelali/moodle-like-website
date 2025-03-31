45ec88145fd43997cde0251345cb8fd528abf5fe
Titouan <titouan.escande@utbm.fr>
2025-03-31 15:25:37 +0200

    Création UE_page.php et UE_page_style.css

---
+++ UE_page.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<?php
+$styles = ['UE_page_style'];
+include("PageParts/header.php");
+?>
+
+
+    <body>
+        <div class="UE_name">
+            <h1>
+                WE4A : Technologies et programmation WEB
+            </h1>
+        </div>
+
+        <div class="Actuality_container_text">
+                <img class="Actuality_icon" width="30" height="30" src="https://img.icons8.com/android/24/speech-bubble.png"
+                     alt="speech-bubble"/>
+                <h3 class="Actuality_title">Lorem Ipsum</h3>
+
+                <!-- L'icône dépend du type de -->
+                <img class="Actuality_icon" title="! Important !" width="30" height="30" src="https://img.icons8.com/emoji/30/double-exclamation-mark-emoji.png"
+                     alt="double-exclamation-mark-emoji"/>
+                <img class="Actuality_icon" title="Information" width="30" height="30" src="https://img.icons8.com/fluency/30/information.png" alt="information"/>
+
+                <a class="Actuality_options" href="#">&#8285;</a> <!-- Lien vers la page pour modifier un post. N'est afficher que si l'utilisateur est un professeur-->
+                <time class="Actuality_time" datetime="2025-0
+                3-31T13:49:57">13h49 le 31 Mars</time>
+
+
+            <br>
+            <p class="text_paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent non viverra enim. Aliquam semper tempus leo non faucibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis egestas tempor ullamcorper. Donec dolor ligula, volutpat nec consectetur eu, cursus a enim. Nullam nibh neque, commodo in lorem rutrum, laoreet interdum elit. Pellentesque eu nunc eu felis tempor eleifend sit amet nec leo. Integer in pharetra mi. Ut dignissim, tortor ac imperdiet fringilla, turpis tortor finibus ligula, in sagittis neque ex eget eros. Mauris congue fringilla arcu, eget placerat arcu imperdiet volutpat. Fusce a posuere sapien.
+
+                Donec ut rutrum neque. Proin pellentesque, massa sit amet euismod rhoncus, arcu quam blandit ex, a tincidunt lectus lectus vitae tellus. Sed vestibulum magna eget sem dictum, a fermentum libero placerat. Nullam posuere tellus et elit tempor, ut sollicitudin enim porttitor. Nulla nec velit at sapien interdum lobortis a et lectus. Praesent eget scelerisque ex. Fusce in turpis et nisl ornare tincidunt eu feugiat purus. Sed sit amet odio convallis, pretium lectus quis, porta lacus. Nullam arcu eros, feugiat eget augue nec, luctus gravida orci. Duis sed ultricies orci. Proin in congue ipsum. Integer dignissim eu turpis ut dignissim. Fusce lacinia erat in orci egestas ullamcorper et quis nisi. Quisque semper libero ac eleifend ornare.
+
+                In hac habitasse platea dictumst. Morbi in tincidunt enim. Proin tempus nulla at dui egestas, ut rutrum erat auctor. Sed sed scelerisque lacus. Praesent urna ipsum, auctor ut finibus non, vulputate eu neque. Etiam imperdiet libero at fermentum efficitur. Aenean in dui nec sapien molestie gravida sit amet blandit lectus. Aenean leo quam, laoreet a ornare in, rhoncus a sapien. Sed maximus mi et turpis tempus, in molestie risus vestibulum. Nunc vehicula, sapien vitae tempor interdum, tellus est finibus ante, ut congue orci mauris eget nisl.
+            </p>
+        </div>
+
+        <hr/>
+
+        <div class="Actuality_container_file">
+
+                <img class="Actuality_icon" width="30" height="30" src="https://img.icons8.com/android/24/speech-bubble.png"
+                     alt="speech-bubble"/>
+                <h3 class="Actuality_title">Lorem Ipsum</h3>
+
+
+
+                <a class="Actuality_options" href="#">&#8285;</a> <!-- Lien vers la page pour modifier un post. N'est afficher que si l'utilisateur est un professeur-->
+                <time class="Actuality_time" datetime="2025-0
+                3-31T13:49:57">13h49 le 31 Mars</time>
+
+                <br>
+                <p class="text_paragraph"> Le descriptif d'un ficher ou une explication de pourquoi il y a le fichier </p>
+
+                <div class="File_box">
+                    <img class="File_icon" width="100" height="100" src="https://img.icons8.com/plasticine/100/file.png" alt="file"/>
+                    <a class="File_text" href="/index.php" download="index">Fichier à télécharger</a>
+                </div>
+
+
+
+
+
+
+
+        </div>
+    </body>
+</html>
+
+
