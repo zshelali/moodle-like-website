c2001a41d1da795814b7b2d9c1715b0fa85e5f69
zshelali <ali.hajeri24@gmail.com>
2025-04-04 01:12:31 +0200

    style

--- admin_home.php
+++ admin_home.php
@@ -5,16 +5,16 @@ include("PageParts/header.php");
 
 <!--BODY-->
 <h3 class="subtitle">Hey, Doe.</h3>
-<div class="button-container">
+<div class="button-container-admin">
     <div class="button-desc">
-        <a href="admin_ue_list.php" class="buttonlink prof-stud-button">UE</a>
+        <a href="admin_ue_list.php" class="buttonlink admin-menu-button">UE</a>
         <br />
         <p class="description"> <!-- à changer -->
             See and edit the list of currently existing UEs, add new ones and assign students and professors to them.
         </p>
     </div>
     <div class="button-desc">
-        <a href="admin_manage_users.php" class="buttonlink prof-stud-button">Users</a>
+        <a href="admin_manage_users.php" class="buttonlink admin-menu-button">Users</a>
         <br />
         <p class="description"> <!-- à changer -->
             See the list of currently registered users, change their name or email address and add new ones.
--- index.php
+++ index.php
@@ -4,24 +4,17 @@ include("PageParts/header.php");
 ?>
 
 <!--BODY-->
-<h3 class="subtitle">Placeholder page.</h3>
+
+<h3 class="main-subtitle">Welcome to the Skooli educational platform.</h3>
 <div class="button-container">
-    <div class="button-desc">
-        <a href="professor.php" class="buttonlink prof-stud-button"
-        >Professor</a
-        >
-        <br />
-        <p class="description"> <!-- à changer -->
-            Quick access to professor.php for debugging.
+    <a href="login.php" class="buttonlink" id="main-signup-btn">Sign in</a>
+    <br />
+    <div class="description-container">
+        <p class="description">
+            Made by Thomas, Mourad, Titouan & Ali
         </p>
     </div>
-    <div class="button-desc">
-        <a href="student.php" class="buttonlink prof-stud-button">Student</a>
-        <br />
-        <p class="description"> <!-- à changer -->
-            Quick access to student.php for debugging.
-        </p>
-    </div>
+
 </div>
 
 <!--BODY END-->
