ca719997502ae0a3ace64a0ab9ff49e647cafdd5
zshelali <ali.hajeri24@gmail.com>
2025-04-07 22:44:07 +0200

    Add jQuery UI tabs styling and enhance header stylesheet link

--- PageParts/header.php
+++ PageParts/header.php
@@ -7,7 +7,7 @@
 
     <!--Common style to all pages (header, footer...) -->
     <link rel="stylesheet" href="style/main_style.css" />
-    <link rel="stylesheet" href="https://code.jquery.com/ui/1.14.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.14.1/themes/base/jquery-ui.css"> 
 
     <!-- JQuery -->
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
